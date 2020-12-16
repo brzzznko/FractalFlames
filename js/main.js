586c78beb85f974b4c2452c471fed66525caa327
Max Borzenko <zzzking999@yandex.ru>
2020-12-16 17:30:36 +0700

    created initial page layout

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext('2d');
+
+function main() {
+    // Adding listener to generate button
+    const generateButton = document.getElementById("generateButton");
+    generateButton.addEventListener("click", () => {});
+}
+
+main()
